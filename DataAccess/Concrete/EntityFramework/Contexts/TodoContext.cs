f431f4dcce8ce4bffa495c24a751cb95fd830d19
Kemal Deniz Lağap <Kemal@DESKTOP-3C9L670>
2024-06-15 15:16:13 +0300

    Creating todo context and completing data access layer

---
+++ DataAccess/Concrete/EntityFramework/Contexts/TodoContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete.EntityFramework.Contexts
+{
+	public class TodoContext: DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer(@"server=DESKTOP-3C9L670;database=db_todos; integrated security=SSPI; TrustServerCertificate=True;");
+		}
+
+		public DbSet<Todo> Todos { get; set; }
+	}
+}
