0795dc71c13a143bea3d69fe2c5bacec7f5e6ce7
Kemal Deniz Lağap <Kemal@DESKTOP-3C9L670>
2024-06-15 14:58:15 +0300

    Writing GetList method for repository pattern

--- Core/DataAccess/Concrete/EntityFramework/EfEntityRepositoryBase.cs
+++ Core/DataAccess/Concrete/EntityFramework/EfEntityRepositoryBase.cs
@@ -40,7 +40,10 @@ namespace Core.DataAccess.Concrete.EntityFramework
 
 		public List<TEntity> GetList(Expression<Func<TEntity, bool>> filter = null)
 		{
-			throw new NotImplementedException();
+			using (TContext context = new TContext())
+			{
+				return filter == null ? context.Set<TEntity>().ToList() : context.Set<TEntity>().Where(filter).ToList();
+			}
 		}
 
 		public void Update(TEntity entity)
